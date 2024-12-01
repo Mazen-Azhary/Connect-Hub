1c0fdc6b4b2e183f176c4ab62adc8d534a546bd3
Omar Adel <omaeradel1999@gmail.com>
2024-12-01 23:04:49 +0200

    Profile page frontend created

---
+++ connecthub/ConnectHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.connecthub;
+
+/**
+ *
+ * @author Etijah
+ */
+public class ConnectHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
