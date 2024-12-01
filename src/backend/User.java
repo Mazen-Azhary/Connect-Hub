1563df1cc03c8f8417dd0ef898e85fffc4245578
Abdelrahman Reda <abdelrahmanredafawzy@gmail.com>
2024-12-01 12:31:01 +0200

    Implemented User class

---
+++ src/backend/User.java
@@ -0,0 +1,82 @@
+package backend;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class User {
+    private String userID;
+    private String email;
+    private String username;
+    private String password;
+    private Date dateOfBirth;
+    private String status;
+    private ArrayList<User> friends = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(String userID, String email, String username, String password, Date dateOfBirth, String status) {
+        this.userID = userID;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+        this.status = status;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public ArrayList<User> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(ArrayList<User> friends) {
+        this.friends = friends;
+    }
+}
