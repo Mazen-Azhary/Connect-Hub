646b63328c0dbce30da7e5e1af399ae9724d1d61
Mohamed Moeen <mohamed.moeen166@gmail.com>
2024-12-02 15:26:42 +0200

    login backend

--- backend/Login.java
+++ backend/Login.java
@@ -1,10 +1,33 @@
 package backend;
 
+import java.security.NoSuchAlgorithmException;
+
 public class Login {
-    public static boolean login(String email,String password)
-    {
+    private static Login instance;
+    private final String filePath = "src/database/Users.json";
+    private  UserDataBase userDataBase;
+    private Login(){
+
+    }
+    public static synchronized Login getInstance() {
+        if (instance == null) {
+            instance = new Login();
+        }
+        return instance;
+    }
+    public boolean login(String email,String password) throws NoSuchAlgorithmException {
         //checking from the data base
         //hashing the password
+        if(userDataBase.contains(email))
+        {
+            User user=userDataBase.search(email);
+            byte[] salt = user.getSalt();
+            if(PasswordHasher.hashedPassword(password,salt).equals(user.getHashedPassword()))
+            {
+                return true;
+            }
+
+        }
         return false;
     }
 }
