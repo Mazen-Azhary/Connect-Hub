07dba12fe9489d07a7dab631160ebae1d96467bb
Mohamed Moeen <mohamed.moeen166@gmail.com>
2024-12-03 18:01:34 +0200

    friend request class is implemented

---
+++ backend/FriendRequest.java
@@ -0,0 +1,37 @@
+package backend;
+
+public class FriendRequest {
+    private String senderId;
+    private String receiverId;
+    private String status;
+
+    public FriendRequest(String senderId, String receiverId, String status) {
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.status = status;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(String receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
