1e09fee52d8976b7f4c815e6c05d8a5d2cc5316d
Mohamed Moeen <mohamed.moeen166@gmail.com>
2024-12-05 13:38:28 +0200

    generate posts and stories

---
+++ backend/ContentViewer.java
@@ -0,0 +1,46 @@
+package backend;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ContentViewer {
+    private UserContentDatabase userContentDatabase = new UserContentDatabase("src/database/UserContents.json");
+    private FriendDatabase friendDatabase = new FriendDatabase("src/database/Friends.json");
+    private static ContentViewer instance;
+    private ContentViewer() {
+    }
+    public synchronized static ContentViewer getInstance() {
+        if(instance ==null) return new ContentViewer();
+        return instance;
+    }
+    public ArrayList<Content> generatePosts(String id) throws IOException {
+        ArrayList<Content> posts = new ArrayList<>();
+        ArrayList<String> friends=friendDatabase.getUser(id).getProfile().getFriends();
+        for (String friend : friends) {
+            User f=userContentDatabase.getUser(friend);
+            for(Content content:f.getProfile().getContents()) {
+                if(content instanceof Post) {
+                    posts.add((Post)content);
+                }
+            }
+        }
+            Collections.shuffle(posts);
+        return posts;
+
+    }
+    public ArrayList<Content> generateStories(String id) throws IOException {
+        ArrayList<Content> stories = new ArrayList<>();
+        ArrayList<String> friends=friendDatabase.getUser(id).getProfile().getFriends();
+        for (String friend : friends) {
+            User f=userContentDatabase.getUser(friend);
+            for(Content content:f.getProfile().getContents()) {
+                if(content instanceof Story) {
+                    stories.add((Story)content);
+                }
+            }
+        }
+        Collections.shuffle(stories);
+        return stories;
+    }
+}
