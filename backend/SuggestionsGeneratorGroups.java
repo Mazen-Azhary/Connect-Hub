76b4f0491a7a25bcaac17d8d7692e82b014629e0
Mohamed Moeen <mohamed.moeen166@gmail.com>
2024-12-10 19:58:13 +0200

    finishing the suggestions in backend

--- backend/SuggestionsGeneratorGroups.java
+++ backend/SuggestionsGeneratorGroups.java
@@ -1,10 +1,14 @@
 package backend;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Map;
 
 public class SuggestionsGeneratorGroups {
     private GroupsDatabase groupsDatabase= new GroupsDatabase("src/database/Groups.json");
     private UserGroupsDatabase userGroupsDatabase= new UserGroupsDatabase("src/database/UserGroups.json");
+    private FriendDatabase friendDatabase= new FriendDatabase("src/database/Friends.json");
     private static SuggestionsGeneratorGroups instance;
     private SuggestionsGeneratorGroups()
     {}
@@ -13,12 +17,37 @@ public class SuggestionsGeneratorGroups {
         if(instance==null) instance=new SuggestionsGeneratorGroups();
         return instance;
     }
-//    public HashSet<Group> generateSuggestions(String id)
-//    {
-//        HashSet<Group> suggestions= new HashSet<>();
-//
-//
-//    }
+    public HashSet<Group> generateSuggestions(String id)
+    {
+        HashSet<Group> suggestions= new HashSet<>();
+        try {
+            ArrayList<String> friends = friendDatabase.getUser(id).getProfile().getFriends();
+            for(String friend: friends)
+            {
+                Map<String,GroupRole> friendGroups=userGroupsDatabase.getUser(friend).getProfile().getGroups();
+                for(Map.Entry<String,GroupRole> entry: friendGroups.entrySet())
+                {
+                    suggestions.add(groupsDatabase.getGroup(entry.getKey()));
+                }
+            }
+            if(suggestions.isEmpty())
+            {
+                int num= groupsDatabase.getMax();
+                for(int i=0;i<num;i++)
+                {
+                    suggestions.add(groupsDatabase.getGroup(i+""));
+                }
+            }
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return suggestions;
+
+
+    }
 
 
 
