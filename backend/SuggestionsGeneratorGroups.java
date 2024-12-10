62b85f6209d74e8783a4fe274de2db5a06202b27
Mohamed Moeen <mohamed.moeen166@gmail.com>
2024-12-10 19:20:04 +0200

    initialaize the groups suggestions

--- backend/SuggestionsGeneratorGroups.java
+++ backend/SuggestionsGeneratorGroups.java
@@ -1,5 +1,25 @@
 package backend;
 
+import java.util.HashSet;
+
 public class SuggestionsGeneratorGroups {
+    private GroupsDatabase groupsDatabase= new GroupsDatabase("src/database/Groups.json");
+    private UserGroupsDatabase userGroupsDatabase= new UserGroupsDatabase("src/database/UserGroups.json");
+    private static SuggestionsGeneratorGroups instance;
+    private SuggestionsGeneratorGroups()
+    {}
+    public synchronized static SuggestionsGeneratorGroups getInstance()
+    {
+        if(instance==null) instance=new SuggestionsGeneratorGroups();
+        return instance;
+    }
+//    public HashSet<Group> generateSuggestions(String id)
+//    {
+//        HashSet<Group> suggestions= new HashSet<>();
+//
+//
+//    }
+
+
 
 }
